e1fdfb1a68db9b644ff6069c40b2d66653720153
Dominik Ingold <dominggold@gmail.com>
2019-04-18 10:59:50 +0200

    player variabeln and methods created

--- src/net/ictcampus/RunFun/Player.java
+++ src/net/ictcampus/RunFun/Player.java
@@ -1,5 +1,20 @@
 package net.ictcampus.RunFun;
 
 public class Player {
+    private String name;
+    private int geschwindigkeit;
+    private String item;
+    private int score;
 
+    public void itemWerfen(String name) {
+
+    }
+
+    public void springen() {
+
+    }
+
+    public void ducken() {
+
+    }
 }
